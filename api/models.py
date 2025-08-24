2d77854864073794e5306afa4e224e910d46a9d6
Carlos Teofilo <carlosneto2810@hotmail.com>
2025-08-23 20:26:17 -0400

    first commit

---
+++ api/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from django.utils import timezone
+
+
+class Receita(models.Model):
+    descricao = models.TextField(blank=False, null=False)
+    valor = models.IntegerField(blank=False, null=False)
+    data = models.DateField(blank=False, null=False)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Despesa(models.Model):
+    descricao = models.TextField(blank=False, null=False)
+    valor = models.IntegerField(blank=False, null=False)
+    data = models.DateField(blank=False, null=False)
+
+    def __str__(self):
+        return self.descricao
